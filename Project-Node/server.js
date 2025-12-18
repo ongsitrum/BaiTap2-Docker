4b530058b9a112057cf06b4f918dc4aef30135c7
Nguyen Thanh Nhan <dh52201141@student.stu.edu.vn>
2025-12-18 10:06:35 -0800

    Cap nhat file cau hinh Docker da chay on dinh

--- Project-Node/server.js
+++ Project-Node/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Xin chao! Day la moi truong Node.js chay tren Docker\n');
+    res.end('Xin chao! Day la Project 2: Node.js chay tren Docker\n');
 });
 server.listen(3000, () => {
     console.log('Server running on port 3000');
